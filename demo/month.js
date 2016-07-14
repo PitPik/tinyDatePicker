5455110d38bb547fb10c14973d20ca3479f7b631
peterd <peter@dematte.at>
2016-07-14 18:05:33 +0200

    some updates

--- demo/month.js
+++ demo/month.js
@@ -1,10 +1,8 @@
 (function(window, events, undefined){
 	'use strict';
 
-	// console.time('Calendar incl. init');
-
 	/* ------------------------------------------------------ */
-	/* -------- initial options for yearly calendar  -------- */
+	/* -------- initial options for monthly calendar  -------- */
 	/* ------------------------------------------------------ */
 	var options = {
 			weekDays: ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'],
@@ -12,62 +10,23 @@
 				'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
 
 			sundayBased: false,
-			renderWeekNo: true,
-			weekDayClass: 'week-day', // not standard: used in template.start
-			weekDayRowHead: '',
 			template: {
+				start: function(month, year) {
+					return '<div class="month">' + this.options.months[month - 1] + ' - ' + year + '</div>' +
+						'<table class="cal-month"><tbody><tr>';
+				},
 				row: '<td class=""><span class=""{{event}} data-day=\'{"day":"{{day}}", "month":"{{month}}", "year":"{{year}}"}\'>{{day-event}}{{today}}</span></td>',
-				start: function(month, year) { // rendering week days in table header
-					var options = this.options,
-						weekDayRow = '<th class="">{{day}}</th>',
-						row = [];
-
-					if (options.renderWeekNo) { // week number head
-						row.push(weekDayRow.replace(/{{day}}/g, options.weekDayRowHead));
-					}
-
-					for (var n = 0, week = 0; n < 7; n++) { // week days
-						week = n + (options.sundayBased ? 0 : (n === 6 ? -6 : 1));
-						row.push(weekDayRow.replace(/class="(.*?)"/, function($1, $2) {
-							return 'class="' + options.weekDayClass + ' ' +
-								(options.workingDays.indexOf(week) === -1 ?
-								options.weekEndClass : '') + '"';
-						}).replace(/{{day}}/g, options.weekDays[week]));
-					}
-					return '<div class="month">' + options.months[month - 1] + ' - ' + year + '</div>' +
-						'<table class="cal-month"><thead><tr>' +
-						row.join('') +
-						'</tr></thead><tbody><tr>';
-				},
-				event: function(day, date, event) { // rendering events
-					var text = [],
-						uuids = [],
-						someExtra = '';
-
-					for (var n = 0, m = event.length; n < m; n++) {
-						event[n].text && text.push('- ' + event[n].text);
-
-						uuids.push(event[n]._id);
-
-						if (event[n].extra) { // extend functionality...
-							someExtra = event[n].extra;
-						}
-					}
-					text = text.join("\n");
-
-					return text ? ' title="' + text + '"' +
-						' data-uuids=\'[' + uuids.join(', ') + ']\'' +
-						(someExtra ? ' data-name="' + someExtra + '"' : '') : '';
-				},
 				day: function(day, date, event) { // rendering every day
 					var text = [];
 
 					for (var n = 0, m = event.length; n < m; n++) {
 						if (event[n].text) {
-							text.push('<span class="single-event">' + event[n].text + '</span>');
+							text.push('<span class="single-event" data-uuid="' + event[n]._id + '">' +
+								event[n].text + '</span>');
 						}
 					}
-					return day + '. ' + this.options.months[date.getMonth()] +
+					return this.options.weekDays[date.getDay()] + '., ' +
+						day + '. ' + this.options.months[date.getMonth()] +
 						text.join('');
 				}
 			},
@@ -80,5 +39,16 @@
 	div.className = 'cal-wrapper month-cal';
 	div.innerHTML = myMonthCalendar.getMonth(2016, 6).html;
 	document.body.appendChild(div);
+
+	/* ------------------------------------------------------ */
+	/* ------------------- event handling  ------------------ */
+	/* ------------------------------------------------------ */
+	// experimental click handler (event delegation) $(div).on('click.cal', '[data-uuid]', function() {...});
+	div.addEventListener('click', function(e) {
+		if (e.target.hasAttribute('data-uuid')) {
+			console.log(myCalendar.options.events[e.target.getAttribute('data-uuid')]);
+			// do something with this data...
+		}
+	});
 
 })(window, window.events);
